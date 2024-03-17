c022b3e259897fb242efc86a224deb966dc94edc
roAscurra <rominaascurra4887@gmail.com>
2024-03-17 14:55:55 -0300

    Login

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function login(){
+    document.getElementById('formLogin').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        //recupero valores de los inputs
+        let user = document.getElementById('user').value;
+        let pass = document.getElementById('pass').value;
+    
+        
+        let url = "http://168.194.207.98:8081/tp/login.php?user="+user+"&&pass="+pass;
+        fetch(url)
+            .then((res) => res.json())
+            .then((response) => {
+                // console.log("Success:", response);
+                respuestaLogin(response);
+            })
+            .catch((error) => console.error("Error:", error));  
+            });
+}
+function respuestaLogin(respuesta){
+    let msjerror = document.getElementById('error');
+    switch(respuesta.respuesta){
+        case "OK": 
+            window.location.href = "lista.html";
+            break;
+        case "ERROR":
+            msjerror.removeAttribute('hidden');
+            break;
+    }
+}
+login()
