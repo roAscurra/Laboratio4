2aeae5bdf468ffc731e27fd9d728e1170fe0d7eb
roAscurra <rominaascurra4887@gmail.com>
2024-04-02 18:39:25 -0300

    Verificacion de valores nulos

--- TP2/ParteA/Node.js/app.js
+++ TP2/ParteA/Node.js/app.js
@@ -8,17 +8,66 @@ const axios = require("axios");
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  password: process.env.DB_PASSWORD
 });
-
 // Conectar a la base de datos
 connection.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
     return;
   }
+
   console.log('Conexión exitosa');
+
+  // Crear la base de datos si no existe
+  connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`, (err) => {
+    if (err) {
+      console.error('Error al crear la base de datos:', err);
+      connection.end(); // Cerrar la conexión en caso de error
+      return;
+    }
+
+    console.log(`Base de datos "${process.env.DB_NAME}" creada correctamente o ya existe`);
+
+    // Seleccionar la base de datos
+    connection.query(`USE ${process.env.DB_NAME}`, (err) => {
+      if (err) {
+        console.error('Error al seleccionar la base de datos:', err);
+        connection.end(); // Cerrar la conexión en caso de error
+        return;
+      }
+
+      console.log(`Base de datos "${process.env.DB_NAME}" seleccionada`);
+
+      // Crear la tabla si no existe
+      const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS Pais (
+          codigoPais INT PRIMARY KEY,
+          nombrePais VARCHAR(50) NOT NULL,
+          capitalPais VARCHAR(50) NOT NULL,
+          region VARCHAR(50) NOT NULL,
+          poblacion BIGINT NOT NULL,
+          latitud DECIMAL(10, 8) NOT NULL,
+          longitud DECIMAL(11, 8) NOT NULL
+        )
+      `;
+      
+      connection.query(createTableQuery, (err) => {
+        if (err) {
+          console.error('Error al crear la tabla:', err);
+          connection.end(); // Cerrar la conexión en caso de error
+          return;
+        }
+
+        console.log('Tabla "Pais" creada correctamente o ya existe');
+
+        // Realizar consultas para obtener los datos de los países
+        getPaises();
+      });
+    });
+  });
+});
+
 
   const getPaises = async () => {
     const promesas = [];
@@ -32,46 +81,54 @@ connection.connect((err) => {
       promesas.push(
         axios.get(`https://restcountries.com/v2/callingcode/${i}`)
           .then(response => {
-            const codigoPais = response.data[0].callingCodes;
-            const nombrePais = response.data[0].name;
-            const capitalPais = response.data[0].capital;
-            const region = response.data[0].region;
-            const poblacion = response.data[0].population;
-            const latitud = response.data[0].latlng[0];
-            const longitud = response.data[0].latlng[1];
-            connection.query(`SELECT * FROM Pais WHERE codigoPais = '${codigoPais}'`, (error, results, fields) => {
-              if (error) {
-                console.error('Error al ejecutar la consulta:', error);
-                return;
-              }
-              if(results.length == 0){
-                inserciones.push(codigoPais);
-                connection.query(`INSERT INTO Pais (codigoPais, nombrePais, capitalPais, region, poblacion, latitud, longitud) 
-                VALUES ('${codigoPais}', '${nombrePais}', '${capitalPais}', '${region}', ${poblacion}, ${latitud}, ${longitud})`,
-                  (error, results, fields) => {
-                    if (error) {
-                      console.error('Error al insertar el país:', error);
-                      return;
-                    }
-                    // console.log(`País ${nombrePais} insertado correctamente.`);
-                    consultasCompletas++;
-                  });
-              }else{
-                updates.push(codigoPais)
-                  // Si el país ya existe, actualizar sus datos
-                  connection.query(`UPDATE Pais SET nombrePais = '${nombrePais}', capitalPais = '${capitalPais}', region = '${region}', poblacion = ${poblacion}, latitud = ${latitud}, longitud = ${longitud} 
-                  WHERE codigoPais = '${codigoPais}'`,
-                  (error, results, fields) => {
-                  if (error) {
-                  console.error('Error al actualizar el país:', error);
+            const data = response.data;
+            if (data[0].callingCodes[0] && data[0].name && data[0].region && data[0].population && data[0].latlng[0] && data[0].latlng[1]) {
+              const codigoPais = response.data[0].callingCodes[0];
+              const nombrePais = response.data[0].name;
+              const capitalPais = response.data[0].capital;
+              const region = response.data[0].region;
+              const poblacion = response.data[0].population;
+              const latitud = response.data[0].latlng[0];
+              const longitud = response.data[0].latlng[1];
+              connection.query(`SELECT * FROM Pais WHERE codigoPais = '${codigoPais}'`, (error, results, fields) => {
+                if (error) {
+                  console.error('Error al ejecutar la consulta:', error);
                   return;
-                  }
-                  // console.log(`País ${nombrePais} actualizado correctamente.`);
-                  consultasCompletas++;
-                  });
-              }
-              // console.log('Resultados de la consulta:', results);
-            });
+                }
+                if(results.length == 0){
+                  inserciones.push(codigoPais);
+                  connection.query(
+                    `INSERT INTO Pais (codigoPais, nombrePais, capitalPais, region, poblacion, latitud, longitud) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                    [codigoPais, nombrePais, capitalPais, region, poblacion, latitud, longitud],
+                    (error, results, fields) => {
+                      if (error) {
+                        console.error('Error al insertar el país:', error);
+                        return;
+                      }
+                      // console.log(`País ${nombrePais} insertado correctamente.`);
+                      consultasCompletas++;
+                    }
+                  );
+                  
+                }else{
+                  updates.push(codigoPais)
+                    // Si el país ya existe, actualizar sus datos
+                    connection.query(
+                      `UPDATE Pais SET nombrePais = ?, capitalPais = ?, region = ?, poblacion = ?, latitud = ?, longitud = ? WHERE codigoPais = ?`,
+                      [nombrePais, capitalPais, region, poblacion, latitud, longitud, codigoPais],
+                      (error, results, fields) => {
+                        if (error) {
+                          console.error('Error al actualizar el país:', error);
+                          return;
+                        }
+                        // console.log(`País ${nombrePais} actualizado correctamente.`);
+                        consultasCompletas++;
+                      }
+                    );                  
+                }
+                // console.log('Resultados de la consulta:', results);
+              });
+            }
           })
           .catch(error => {
             sinDatos.push(i)
@@ -105,7 +162,3 @@ connection.connect((err) => {
     });
   };
   
-  getPaises();
-  
-});
-
